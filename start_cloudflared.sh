fb2b1791152942b6c222fe95efbc72b0f5acc499
A-Akhil <akhilrahul70@gmail.com>
2024-12-04 22:56:21 +0530

    Sync changes: Wed 04 Dec 2024 10:56:21 PM IST

--- start_cloudflared.sh
+++ start_cloudflared.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting Cloudflared Tunnel..."
-~/serverport/server-dgx/cloudflared tunnel --url http://localhost:8080 &
+~/serverport/server-2/cloudflared tunnel --url http://localhost:8080 &
